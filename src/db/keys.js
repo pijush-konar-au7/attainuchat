52008d8c9a96fd11240746ae74a4464a474ed180
Pijush Konar <59543014+pijush-konar-au7@users.noreply.github.com>
2020-06-24 21:07:22 +0530

    First Commit

---
+++ src/db/keys.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+//Db connection
+
+mongoose.connect(`mongodb+srv://${process.env.NAME}`+ encodeURIComponent(process.env.PASSWORD) + `${process.env.CLUSTER}.mongodb.net/attainu-chat?retryWrites=true`, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+
+
+//connect local
+
+// mongoose.connect('mongodb://localhost:27017/chat2', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// })
+// .then(() => console.log('MongoDB Connected'))
+// .catch(err => console.log(err));
